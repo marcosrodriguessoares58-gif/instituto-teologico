1fbe809d92497e574eb09e084aa82ba9deb09b55
Seu Nome <seu-email@exemplo.com>
2026-05-22 13:00:05 -0300

    Correcao final do caminho base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- ESSA LINHA É A CHAVE
+  base: './', // Esta linha corrige o caminho dos ficheiros para o Vercel
 })
