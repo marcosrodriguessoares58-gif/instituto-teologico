618e2c3c243c8003434b25d35aef636a42b1fe20
Seu Nome <seu-email@exemplo.com>
2026-05-22 21:33:08 -0300

    ajuste fino de proporcao contida

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
-function App() {
-  return (
-    <>
-      {/* Container do Banner */}
-      <div className="banner-container"></div>
-
-      {/* Estilos CSS */}
-      <style>{`
-        body, html { 
-          margin: 0; 
-          padding: 0; 
-          width: 100%; 
-          min-height: 100%; 
-        }
-        .banner-container {
-          width: 100%;
-          height: 90vh;
-          background-image: url('/arte-pequena.jpg');
-          background-size: cover;
-          background-position: center top;
-          background-repeat: no-repeat;
-        }
-      `}</style>
-    </>
-  );
-}
-
-export default App;
+<style>{`
+  body, html { 
+    margin: 0; 
+    padding: 0; 
+    width: 100%; 
+    min-height: 100%; 
+    background-color: #000; /* Cor de fundo caso a imagem fique menor que a tela */
+  }
+  .banner-container {
+    width: 100%;
+    height: 80vh; /* Ajuste para caber melhor na tela */
+    background-image: url('/arte-pequena.jpg');
+    background-size: contain; /* Mostra a imagem inteira sem cortar */
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+`}</style>
