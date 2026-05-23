7c79c596e1812f3ba6e5b751e62ddaee7d240316
Seu Nome <seu-email@exemplo.com>
2026-05-22 21:31:04 -0300

    ajuste final de proporção do banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 function App() {
   return (
     <>
+      {/* Container do Banner */}
       <div className="banner-container"></div>
+
+      {/* Estilos CSS */}
       <style>{`
-        body, html { margin: 0; padding: 0; width: 100%; min-height: 100%; }
+        body, html { 
+          margin: 0; 
+          padding: 0; 
+          width: 100%; 
+          min-height: 100%; 
+        }
         .banner-container {
           width: 100%;
-          height: 50vh;
+          height: 90vh;
           background-image: url('/arte-pequena.jpg');
-          background-size: contain;
-          background-position: center;
+          background-size: cover;
+          background-position: center top;
           background-repeat: no-repeat;
         }
       `}</style>
     </>
   );
 }
+
 export default App;
