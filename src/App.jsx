796cc95260d3f2e9aa5fb44c4d59d8a856fd11ce
Seu Nome <seu-email@exemplo.com>
2026-05-22 22:49:09 -0300

    Imagem esticada para preencher tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-{/* SEÇÃO DA ARTE: Tela cheia sem bordas */}
 <div style={{ 
   width: '100vw',
   height: '100vh',
   margin: 0,
   padding: 0,
-  overflow: 'hidden',
-  position: 'relative'
+  overflow: 'hidden'
 }}>
   <img 
     src="/arte-principal.jpg" 
     alt="Capa do Instituto Teológico" 
     style={{ 
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      width: '100%',
-      height: '100%',
-      objectFit: 'cover',
+      width: '100vw',
+      height: '100vh',
+      objectFit: 'fill',
       display: 'block'
     }} 
   />
