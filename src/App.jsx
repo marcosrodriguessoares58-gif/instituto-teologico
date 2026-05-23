001f16ad3c3e421a6609278e5f8c228b380206ab
Seu Nome <seu-email@exemplo.com>
2026-05-22 22:47:43 -0300

    Imagem principal cobrindo tela cheia

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-{/* SEÇÃO DA ARTE: Tela cheia */}
+{/* SEÇÃO DA ARTE: Tela cheia sem bordas */}
 <div style={{ 
   width: '100vw',
   height: '100vh',
   margin: 0,
   padding: 0,
-  overflow: 'hidden'
+  overflow: 'hidden',
+  position: 'relative'
 }}>
   <img 
     src="/arte-principal.jpg" 
     alt="Capa do Instituto Teológico" 
     style={{ 
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
       width: '100%',
       height: '100%',
       objectFit: 'cover',
